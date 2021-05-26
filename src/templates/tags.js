7c85dba781f32825baae9942b2be186886e7b479
Allison Lu <allisonlu820@gmail.com>
2021-05-25 21:25:10 -0700

    Tags page: - added styles - differentiated blog posts from events - added Emoji component for Tags page

--- src/templates/tags.js
+++ src/templates/tags.js
@@ -2,38 +2,61 @@ import React from 'react'
 import { Helmet } from 'react-helmet'
 import { Link, graphql } from 'gatsby'
 import Layout from '../components/Layout'
+import Emoji from '../components/Emoji'
+import './tags.scss'
 
 class TagRoute extends React.Component {
   render() {
     const posts = this.props.data.allMarkdownRemark.edges
     const postLinks = posts.map((post) => (
-      <li key={post.node.fields.slug}>
-        <Link to={post.node.fields.slug}>
-          <h2 className="is-size-2">{post.node.frontmatter.title}</h2>
-        </Link>
+      <li
+        key={post.node.fields.slug}
+        className="tags__result px-5 mb-4"
+      >
+        <span className="tags__result-type mr-4">
+          { post.node.frontmatter.templateKey === 'event-single' ?  <Emoji symbol="🗓" label="calendar"/> :
+          <Emoji symbol="✏️" label="pencil"/> }
+        </span>
+
+        <span>{post.node.frontmatter.date}</span>
+
+        <h2 className="is-size-4 tags__result-title">
+          <Link
+            to={post.node.fields.slug}
+            className="tags__result-link"
+          >
+            {post.node.frontmatter.title}
+          </Link>
+        </h2>
+
+        <span className="section-divider--rainbow"></span>
+
       </li>
     ))
     const tag = this.props.pageContext.tag
     const title = this.props.data.site.siteMetadata.title
     const totalCount = this.props.data.allMarkdownRemark.totalCount
-    const tagHeader = `${totalCount} post${
+    const tagHeader = `${totalCount} result${
       totalCount === 1 ? '' : 's'
     } tagged with “${tag}”`
 
     return (
       <Layout>
-        <section className="section">
+        <section className="section tags">
           <Helmet title={`${tag} | ${title}`} />
-          <div className="container content">
+          <div className="container">
             <div className="columns">
               <div
                 className="column is-10 is-offset-1"
                 style={{ marginBottom: '6rem' }}
               >
-                <h3 className="title is-size-4 is-bold-light">{tagHeader}</h3>
-                <ul className="taglist">{postLinks}</ul>
+                <span className="is-size-6 is-bold-light">{tagHeader}</span>
+                <ul className="tags__list py-5">{postLinks}</ul>
                 <p>
-                  <Link to="/tags/">Browse all tags</Link>
+                  <Link
+                    to="/tags/"
+                    className="tags__all"
+                  >Browse all tags</Link>
                 </p>
               </div>
             </div>
@@ -65,6 +88,8 @@ export const tagPageQuery = graphql`
             slug
           }
           frontmatter {
+            templateKey
+            date(formatString: "MMMM DD, YYYY")
             title
           }
         }
