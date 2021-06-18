ad2591948016be9551212d7d6c3e608361c6552b
Allison Lu <allisonlu820@gmail.com>
2021-06-18 14:07:59 -0700

    append to last commit lol

--- src/pages/events/index.js
+++ src/pages/events/index.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import Layout from '../../components/Layout'
 import EventList from '../../components/EventList'
+import Calendar from '../../components/Calendar'
 
 export default class EventsPage extends React.Component {
   render() {
@@ -20,16 +21,7 @@ export default class EventsPage extends React.Component {
         </div>
         <section className="section">
           <div className="container">
-
-            <iframe
-              src="https://calendar.google.com/calendar/embed?height=600&amp;wkst=1&amp;bgcolor=%23ffffff&amp;ctz=America%2FLos_Angeles&amp;src=cXVlZXJsYW5kY2VudGVyQGdtYWlsLmNvbQ&amp;src=YWRkcmVzc2Jvb2sjY29udGFjdHNAZ3JvdXAudi5jYWxlbmRhci5nb29nbGUuY29t&amp;src=NzkwbjliMDZmOHNubWFyaWFqdWZ1NjRrbjRAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ&amp;src=ZW4udXNhI2hvbGlkYXlAZ3JvdXAudi5jYWxlbmRhci5nb29nbGUuY29t&amp;color=%23039BE5&amp;color=%2333B679&amp;color=%23A79B8E&amp;color=%230B8043"
-              style={{border:"solid 1px #777"}}
-              width="800"
-              height="600"
-              frameborder="0"
-              scrolling="no"
-              ></iframe>
-
+            <Calendar />
             <EventList />
           </div>
         </section>
