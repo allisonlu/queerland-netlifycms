e5cd25d417d7b22dca51c480855715ca55382d71
Allison Lu <allisonlu820@gmail.com>
2021-06-21 14:58:50 -0700

    Updated privacy and display options for Google Calendar

--- src/components/Calendar.js
+++ src/components/Calendar.js
@@ -5,7 +5,7 @@ const Calendar = () => {
     <iframe
       className="gcalendar column is-10 is-offset-1"
       title="QueerLand Event calendar"
-      src="https://calendar.google.com/calendar/embed?height=600&amp;wkst=1&amp;bgcolor=%23ffffff&amp;ctz=America%2FLos_Angeles&amp;src=cXVlZXJsYW5kY2VudGVyQGdtYWlsLmNvbQ&amp;src=YWRkcmVzc2Jvb2sjY29udGFjdHNAZ3JvdXAudi5jYWxlbmRhci5nb29nbGUuY29t&amp;src=NzkwbjliMDZmOHNubWFyaWFqdWZ1NjRrbjRAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ&amp;src=ZW4udXNhI2hvbGlkYXlAZ3JvdXAudi5jYWxlbmRhci5nb29nbGUuY29t&amp;color=%23039BE5&amp;color=%2333B679&amp;color=%23A79B8E&amp;color=%230B8043"
+      src="https://calendar.google.com/calendar/embed?height=600&amp;wkst=1&amp;bgcolor=%23ffffff&amp;ctz=America%2FLos_Angeles&amp;src=NzkwbjliMDZmOHNubWFyaWFqdWZ1NjRrbjRAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ&amp;src=ZW4udXNhI2hvbGlkYXlAZ3JvdXAudi5jYWxlbmRhci5nb29nbGUuY29t&amp;color=%23A79B8E&amp;color=%230B8043&amp;showTitle=0&amp;showNav=1&amp;showPrint=0&amp;showDate=1&amp;showCalendars=0&amp;showTz=0&amp;showTabs=0"
       height="600"
       frameborder="0"
       scrolling="no"
