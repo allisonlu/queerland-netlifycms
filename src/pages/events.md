b7b6da4e6329d4b8acf27fe8cafb480c05d0a7ad
Allison Lu <allisonlu820@gmail.com>
2021-09-06 23:20:36 -0700

    created event page template to include editable hero image

---
+++ src/pages/events.md
@@ -0,0 +1,6 @@
+---
+title: Events Landing page
+templateKey: event-page
+image: /img/img_3883.jpg
+---
+lorem ipsum test text body
---
+++ src/templates/event-page.js
@@ -0,0 +1,92 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import { graphql } from 'gatsby'
+import Layout from '../components/Layout'
+import Content, { HTMLContent } from '../components/Content'
+import EventList from '../components/EventList'
+import DisplayCalendar from '../components/DisplayCalendar'
+
+
+export const EventPageTemplate = ({ title, image, body, contentComponent }) => {
+  const PageContent = contentComponent || Content
+
+  return (
+    <div>
+
+      {image != null ? (
+        <div 
+          className="hero-container mt-0"
+          >
+            <img src= {`${ image.publicURL }`} />
+        </div>
+      ) : null
+      }
+
+      <section className="section section--gradient">
+        <div className="container">
+          <div className="columns">
+            <div className="column is-10 is-offset-1">
+
+                <h2 className="title is-size-2 has-text-weight-bold is-bold-light ribbon">
+                  {title}
+                </h2>
+
+                <PageContent className="content" content={body} />
+
+                <DisplayCalendar />
+
+                <span className="section-divider--rainbow"></span>
+
+                <EventList />
+
+            </div>
+          </div>
+        </div>
+      </section>
+
+    </div>
+  )
+}
+
+EventPageTemplate.propTypes = {
+  title: PropTypes.string.isRequired,
+  image: PropTypes.string,
+  body: PropTypes.string,
+  contentComponent: PropTypes.func,
+}
+
+const EventPage = ({ data }) => {
+  const { markdownRemark: post } = data
+
+  return (
+    <Layout>
+      <EventPageTemplate
+        contentComponent={HTMLContent}
+        title={post.frontmatter.title}
+        image={post.frontmatter.image}
+        body={post.html}
+      />
+    </Layout>
+  )
+}
+
+EventPage.propTypes = {
+  data: PropTypes.object.isRequired,
+}
+
+export default EventPage
+
+export const eventPageQuery = graphql`
+  query EventPage($id: String!) {
+    markdownRemark(id: { eq: $id }) {
+      html
+      frontmatter {
+        title
+        templateKey
+        image {
+          publicURL
+        }
+      }
+    }
+  }
+`
